3f5ac316d0c582c9265aa5907495c2c3b33a1ba0
Aditi-s26 <24aiml060@bnmit.in>
2026-04-07 09:39:29 +0530

    Update main.py

--- web_app/main.py
+++ web_app/main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
 @app.route('/')
 def home():  # Changed semicolon (;) to a colon (:)
     # render_template automatically looks in the /templates folder
