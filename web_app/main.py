efed6c43fa052f570f8858b81e869b9535e6f661
Aditi-s26 <24aiml060@bnmit.in>
2026-04-09 15:36:51 +0530

    Update main.py

--- web_app/main.py
+++ web_app/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
-def home():  # Changed semicolon (;) to a colon (:)
+def home():
     # render_template automatically looks in the /templates folder
     return render_template('index.html', title="Home Page")
 
-if __name__ == '__main__':  # Added missing underscores and fixed 'main'
+if __name__ == '__main__':
     # Local development server
-    app.run(host='127.0.0.1', port=8080, debug=True) # Fixed the IP address (127.0.0.1)
+    # debug=True allows the server to reload when you make changes
+    app.run(host='127.0.0.1', port=8080, debug=True)
